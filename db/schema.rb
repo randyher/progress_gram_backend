1f050f9c260676fd638d5df80a935bd0fee7c6ea
github username <randyherasme6@gmail.com>
2019-10-17 10:09:34 -0400

    add bio

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_09_30_144331) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
